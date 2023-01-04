7d6db7f7037881ea5752dd2f9ce0baaceed68697
ericrdrew <erdrew@ncsu.edu>
2023-01-04 14:40:26 -0500

    initial commit for shooting plots

---
+++ LakersShooting.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 13 21:05:23 2022
+LAKERS PLOTS
+@author: ericd
+"""
+
+############################################## LAKERS PLOT ###############################
+#create plot area
+fig, ax = plt.subplots()
+
+#set plot size
+fig.set_size_inches(7,5)
+
+lakers = pd.read_csv('lakersPerGame.csv')
+
+lakers.head()
+
+lakersShooting = pd.read_csv('lakersShooting.csv')
+lakersShooting.head()
+
+lakers = pd.merge(lakers, lakersShooting, on=['Player'])
+
+lakers = lakers[lakers.Player != 'Isaiah Thomas']
+lakers = lakers[lakers.Player != 'Darren Collison']
+lakers = lakers[lakers.Player != 'Chaundee Brown Jr.']
+lakers = lakers[lakers.Player != 'Sekou Doumbouya']
+lakers = lakers[lakers.Player != 'Jemerrio Jones']
+lakers = lakers[lakers.Player != 'Mason Jones']
+
+#add path column
+lakers['path'] = 'C:/Users/ericd/OneDrive - North Carolina State University/Desktop/WNBA Viz/Lakers/'+ lakers['Player']  + '.png'
+
+
+#headshots
+def getImage(path):
+    return OffsetImage(plt.imread(path), zoom=0.16, alpha=1)
+
+
+#mid range percentage FGA and FG% plot
+fig, ax = plt.subplots(figsize=(6, 4), dpi=600)
+ax.scatter(lakers['10-16Perc'],lakers['10-16Att'], color='white')
+ax.set_title('Mid-Range Shooting, Los Angeles Lakers, 2022', size=10)
+ax.set_ylabel('Percent of shots taken that are midrange')
+ax.set_xlabel('Midrange shooting percentage')
+
+
+
+for index, row in lakers.iterrows():
+    ab = AnnotationBbox(getImage(row['path']), (row['10-16Perc'], row['10-16Att']), frameon=False)
+    ax.add_artist(ab)
